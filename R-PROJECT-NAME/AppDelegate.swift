ede7e5d989028cb8861201529586ffa48f4dcaf8
Håvard Fossli <hfossli@gmail.com>
2017-02-04 16:04:27 +0100

    Initial commit

---
+++ R-PROJECT-NAME/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  R-PROJECT-NAME
+//
+//  Created by R-AUTHOR-NAME.
+//  Copyright © 2017 R-COMPANY-NAME. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ R-PROJECT-NAME/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  R-PROJECT-NAME
+//
+//  Created by R-AUTHOR-NAME.
+//  Copyright © 2017 R-COMPANY-NAME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
