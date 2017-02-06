edd8f9fc1caa1d239f0329627b9f5b78d667359c
Håvard Fossli <hfossli@gmail.com>
2017-02-06 14:28:24 +0100

    Fastlane

--- R-PROJECT-NAME/AppConstants.swift
+++ R-PROJECT-NAME/AppConstants.swift
@@ -3,7 +3,14 @@
 //   MAINTAINED BY fastlane/Fastfile. Any changes to this *file* will be removed and overwritten.   //
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-APP_APP_ICON = AppIconStaging
+class AppConstants {
+
+  /* Before debugging make sure you did run `fastlane switch -e dev` in order to update this file. */
+
+  static let server = "netthandel-test.applab.no"
+  static let serverUrl = "https://netthandel-test.applab.no/"
+  static let hockeyAppIdentifier = get_env("APP_HOCKEYAPP_ID")
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //   MAINTAINED BY fastlane/Fastfile. Any changes to this *file* will be removed and overwritten.   //
